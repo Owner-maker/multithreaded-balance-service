da89f310b2146c2ffd0356cc988673a69b974f95
Owner-maker <lisitsyn.artem.d@gmail.com>
2022-12-15 06:12:50 +0400

    initial commit

---
+++ src/main/java/ru/lisitsyn/multithreadedbalanceservice/MultithreadedBalanceServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.lisitsyn.multithreadedbalanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadedBalanceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadedBalanceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lisitsyn/multithreadedbalanceservice/MultithreadedBalanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lisitsyn.multithreadedbalanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadedBalanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
